9c36f9bba52e66b4b7af6e75f18828cffbe7acf6
griffinkelly <griffinkelly2013@u.northwestern.edu>
2016-06-15 00:07:07 -0500

    Get stopwatch working with highlighting green. Change storyboard to include constraints for iPhone

--- CustomCell.m
+++ CustomCell.m
@@ -8,27 +8,66 @@
 
 #import "CustomCell.h"
 
+static NSInteger const kCustomEditControlWidth=42;
+
+@interface CustomCell ()
+
+@property (nonatomic, getter=isPseudoEditing) BOOL pseudoEdit;
+@property (nonatomic, getter=isDeleting) BOOL deleting;
+
+
+@end
+
 @implementation CustomCell
 
-- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
-{
-    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
-    if (self) {
-        // Initialization code
+- (void)setEditing:(BOOL)editing animated:(BOOL)animated {
+        NSLog(@"%hhd",[self.delegate isPseudoEditing]);
+    if ([self.delegate isPseudoEditing]) {
+        self.pseudoEdit = editing;
+        [self beginEditMode];
+        [self.sw startWatch];
+    } else {
+        [super setEditing:editing animated:animated];
+        [self beginEditMode];
+        [self.sw stopWatch];
     }
-    return self;
 }
 
-- (void)awakeFromNib
-{
-    // Initialization code
-}
-
-- (void)setSelected:(BOOL)selected animated:(BOOL)animated
-{
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
     [super setSelected:selected animated:animated];
+    if (selected)
+        [self.sw stopWatch];
+    else
+        [self.sw startWatch];
+    [self.sw setHighlighted:selected];
+    //self.customEditControl.selected = selected;
+}
 
-    // Configure the view for the selected state
+#pragma mark - Public API
+- (void)configureCell {
+    CGFloat swDim = CGRectGetHeight(self.frame) - 16;
+    self.sw = [[StopWatchControl alloc] initWithFrame:CGRectMake(8, 8, swDim, swDim)];
+    [self.customEditControl addSubview:self.sw];
+}
+
+#pragma mark - Cell custom edit control Action
+
+- (IBAction)customEditControlPressed:(id)sender {
+    // [self setSelected:YES animated:YES];
+    [self.delegate selectCell:self];
+}
+
+
+#pragma mark - Private Method
+
+// Animate view to show/hide custom edit control/button
+- (void)beginEditMode {
+    //NSLog(@"%hhd",self.pseudoEdit);
+    self.leadingSpaceMainViewConstraint.constant = [self.delegate isPseudoEditing] ? 0 : -42;
+    
+    [UIView animateWithDuration:0.3 animations:^{
+        [self.superview layoutIfNeeded];
+    }];
 }
 
 @end
